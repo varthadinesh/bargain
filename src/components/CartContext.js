50c0efd7adf45aa3c042733c258031dfbdf7b5f6
varthadinesh <30522988+varthadinesh@users.noreply.github.com>
2024-02-20 19:25:30 +0530

    Add files via upload

---
+++ src/components/CartContext.js
@@ -0,0 +1,135 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+const CartContext = createContext();
+
+export const useCart = () => useContext(CartContext);
+
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+  const [wishItems, setWishItems] = useState([]);
+  const [selectedWishlistItems, setSelectedWishlistItems] = useState([]);
+
+  const [user, setUser] = useState(() => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    return storedUser || { firstname: "", lastname: "", email: "" };
+  });
+
+  const setUserData = (userData) => {
+    setUser(userData);
+  };
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
+
+  const addToCart = (product) => {
+    const exists = cartItems.some(item => item.id === product.id);
+    
+    if (exists) {
+        alert('Product already exists in cart');
+        return;
+    }
+
+    setCartItems(prevItems => [...prevItems, { ...product }]);
+    alert('Product Added to cart')
+};
+
+
+  const removeFromCart = (productId) => {
+    setCartItems((prevItems) =>
+      prevItems.filter((item) => item.id !== productId)
+    );
+    alert('Product removed from cart')
+  };
+
+
+
+  const incrementQuantity = (index) => {
+    const updatedCartItems = [...cartItems];
+    updatedCartItems[index].quantity++;
+    setCartItems(updatedCartItems);
+  };
+
+  const decrementQuantity = (index) => {
+    const updatedCartItems = [...cartItems];
+    if (updatedCartItems[index].quantity > 1) {
+      updatedCartItems[index].quantity--;
+      setCartItems(updatedCartItems);
+    }
+  };
+
+  const calculateTotalPrice = () => {
+    return cartItems.reduce(
+      (total, item) => total + item.price,
+      0
+    );
+  };
+
+  const addToWishlist = (product) => {
+    const exists = wishItems.some(item => item.id === product.id);
+    
+    if (exists) {
+        alert('Product already exists in wishlist');
+        return;
+    }
+
+    setWishItems((prevItems) => [...prevItems, product]);
+    alert('Product Added to Wishlist')
+  };
+
+  const removeFromWishlist = (productId) => {
+    setWishItems((prevItems) =>
+      prevItems.filter((item) => item.id !== productId)
+    );
+    alert('Product removed from cart')
+
+  };
+
+
+  const moveFromWishlistToCart = () => {
+    selectedWishlistItems.forEach((productId) => {
+      const product = wishItems.find((item) => item.id === productId);
+      if (product) {
+        addToCart(product);
+        removeFromWishlist(productId);
+      }
+    });
+    setSelectedWishlistItems([]);
+  };
+
+  const handleCheckboxChange = (productId) => {
+    if (selectedWishlistItems.includes(productId)) {
+      setSelectedWishlistItems(
+        selectedWishlistItems.filter((id) => id !== productId)
+      );
+    } else {
+      setSelectedWishlistItems([...selectedWishlistItems, productId]);
+    }
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        user,
+        setUserData,
+        cartItems,
+        addToCart,
+        removeFromCart,
+        incrementQuantity,
+        decrementQuantity,
+        calculateTotalPrice,
+        wishItems,
+        addToWishlist,
+        removeFromWishlist,
+        moveFromWishlistToCart,
+        selectedWishlistItems,
+        handleCheckboxChange,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export function useData() {
+  return useContext(CartContext);
+}
---
+++ src/components/home.js
@@ -0,0 +1,72 @@
+import React from "react";
+import MyNavbar from "./navbar";
+import Footer from "./footer";
+import Curosel from "./curosal";
+import CarouselComponent from "./carouselcomponent";
+// import Carousel from "react-elastic-carousel";
+import productList from './Products.json';
+import Product from "./Product";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+// const breakPoints = [
+//   { width: 1, itemsToShow: 1 },
+//   { width: 550, itemsToShow: 2 },
+//   { width: 768, itemsToShow: 3 },
+//   { width: 992, itemsToShow: 3 },
+//   { width: 1200, itemsToShow: 3 }
+// ];
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 1600, min: 1201 },
+    items: 4
+  },
+  desktop: {
+    breakpoint: { max: 1200, min: 992 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 991, min: 768 },
+    items: 3
+  },
+  mobile: {
+    breakpoint: { max: 767, min: 0 },
+    items: 1
+  }
+};
+
+const Home = () => {
+  return (
+    <>
+      <MyNavbar />
+      <CarouselComponent />
+
+      <h2 className="text-center p-2">Product Categories</h2>
+      <Curosel />
+      <h2 className="text-center p-2">Featured Products</h2>
+      <div className="mt-4">
+        {/* <Carousel breakPoints={breakPoints}>
+        {productList.map((product, index) => (
+          <div className="">
+              <Product product={product}  key={index}/>
+            </div>
+            ))}
+        </Carousel> */}
+        <Carousel 
+        responsive={responsive}
+        // showDots={true}
+        >
+        {productList.map((product, index) => (
+          <div className="m-3">
+              <Product product={product}  key={index}/>
+            </div>
+            ))}
+        </Carousel>;
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
